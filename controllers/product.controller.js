ee4f89d8ab61d5c61fbe628f63feab5d79f17fcf
Fagbayibo <fagbayibooluwasegun@gmail.com>
2025-04-27 01:31:32 +0100

    Feat: Added Github, Amazon deleting style to the product

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -25,7 +25,7 @@ export const getAllProduct = async (req, res) => {
 export const getSingleeProduct = async (req, res) => {
   try {
     const productID = req.params.id;
-    const getProductFromID = await Product.findOne(productID);
+    const getProductFromID = await Product.findById(productID);
     if (!getProductFromID) {
       res.status(404).json({
         success: false,
@@ -71,5 +71,60 @@ export const createProduct = async (req, res) => {
     });
   }
 };
-export const updateProduct = async (req, res) => {};
-export const deleteProduct = async (req, res) => {};
+export const updateProduct = async (req, res) => {
+  try {
+    const productID = req.params.id;
+    const updatePayload = req.body;
+    const findProduct = await Product.findByIdAndUpdate(productID, updatePayload, {
+      new: true,
+    });
+    if (!findProduct) {
+      res.status(404).json({
+        success: false,
+        message: "Product not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Product updated successfully",
+      data: findProduct,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: `Failed to create product: ${error.message}`,
+    });
+  }
+};
+export const deleteProduct = async (req, res) => {
+  try {
+    const productID = req.params.id;
+    const {name} = req.body;
+    const product = await Product.findById(productID);
+    if (!product) {
+      res.status(404).json({
+        success: false,
+        message: `Product not found`,
+      });
+    }
+
+    if (product.name !== name) {
+      res.status(400).json({
+        success: false,
+        message: `Product name mismatch. Cannot delete`,
+      });
+    }
+
+    await Product.findByIdAndDelete(productID);
+    res.status(200).json({
+      success: true,
+      message: `Product delete successfully`,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: `Failed to delete product: ${error.message}`,
+    });
+  }
+};
